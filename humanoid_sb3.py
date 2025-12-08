ebbe2d686963010f355dcad4526f62d47dea3b73
LucaFrat <luca_frattini@outlook.com>
2025-12-08 11:00:53 +0100

    add testing to the sb3 environment

--- humanoid_sb3.py
+++ humanoid_sb3.py
@@ -13,19 +13,29 @@ os.makedirs(model_dir, exist_ok=True)
 os.makedirs(logs_dir, exist_ok=True)
 
 device = "cpu"
+TIMESTEPS = 25_000_000
+ITERATIONS = 10
 
-def train(env):
+def train():
+    env = make_vec_env("Humanoid-v5", n_envs=12, vec_env_cls=SubprocVecEnv)
     model = PPO('MlpPolicy', env=env, verbose=1, device=device, tensorboard_log=logs_dir)
-    TIMESTEPS = 500_000
+    local_timesteps = TIMESTEPS//ITERATIONS
     iters = 0
-    for _ in range(10):
+    for _ in range(ITERATIONS):
         iters += 1
-        model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False)
-        model.save(f"{model_dir}/PPO_{TIMESTEPS*iters}")
+        model.learn(total_timesteps=local_timesteps, reset_num_timesteps=False)
+        model.save(f"{model_dir}/PPO_{local_timesteps*iters}")
 
+def test():
+    env = make_vec_env("Humanoid-v5", n_envs=4, vec_env_cls=SubprocVecEnv)
+
+    model = PPO.load(f"{model_dir}/PPO_{TIMESTEPS}")
+    obs = env.reset()
+    while True:
+        action, _states = model.predict(obs)
+        obs, rewards, terminated, truncated = env.step(action)
+        env.render("human")
 
 
 if __name__ == "__main__":
-    env = make_vec_env("Humanoid-v5", n_envs=12, vec_env_cls=SubprocVecEnv)
-
-    train(env=env)
+    train()
