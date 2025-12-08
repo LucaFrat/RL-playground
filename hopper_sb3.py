1041952eb7fdddae01787c92880002f8d4ba3780
LucaFrat <luca_frattini@outlook.com>
2025-12-08 15:38:09 +0100

    humanoid and hopper are stable in training with sb3

--- hopper_sb3.py
+++ hopper_sb3.py
@@ -9,7 +9,7 @@ import os
 
 
 model_dir = "models"
-logs_dir = "logs"
+logs_dir = "logs/hopper"
 stats_path = os.path.join(model_dir, "vec_normalize.pkl")
 
 os.makedirs(model_dir, exist_ok=True)
@@ -17,20 +17,14 @@ os.makedirs(logs_dir, exist_ok=True)
 
 
 device = "cpu"
-TIMESTEPS = 25_000_000
+TIMESTEPS = 2_000_000
 ITERATIONS = 10
 
 
 def train():
 
-    env = make_vec_env("Humanoid-v5", n_envs=12, vec_env_cls=SubprocVecEnv)
+    env = make_vec_env("Hopper-v5", n_envs=8, vec_env_cls=SubprocVecEnv)
     env = VecNormalize(env, norm_obs=True, norm_reward=True, clip_obs=10.)
-
-    # define a larger network architecture
-    policy_kwargs = dict(
-        net_arch=dict(pi=[512, 512], vf=[256, 256]),
-        activation_fn=nn.Tanh
-    )
 
     model = PPO(
         'MlpPolicy',
@@ -38,10 +32,7 @@ def train():
         verbose=1,
         device=device,
         tensorboard_log=logs_dir,
-        policy_kwargs=policy_kwargs,
-        batch_size=256,
-        n_steps=2048,
-        learning_rate=3e-4,
+        batch_size=128,
         target_kl=0.03
     )
 
@@ -52,16 +43,17 @@ def train():
         iters += 1
         model.learn(total_timesteps=local_timesteps, reset_num_timesteps=False)
 
-        model.save(f"{model_dir}/PPO_{local_timesteps*iters}")
+        model.save(f"{model_dir}/PPO_Hopper_{local_timesteps*iters}")
         env.save(stats_path)
 
+
 def test():
-    env = make_vec_env("Humanoid-v5", n_envs=1, vec_env_cls=SubprocVecEnv)
+    env = make_vec_env("Hopper-v5", n_envs=1, vec_env_cls=SubprocVecEnv)
     env = VecNormalize.load(stats_path, env)
     env.training = False
     env.norm_reward = False
 
-    model = PPO.load(f"{model_dir}/PPO_{TIMESTEPS}")
+    model = PPO.load(f"{model_dir}/PPO_Hopper_{TIMESTEPS}")
 
     obs = env.reset()
     while True:
@@ -72,4 +64,4 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    train()
